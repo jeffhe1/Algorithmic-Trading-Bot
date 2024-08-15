7e858808cefcd65121f1972052324efe7cd050e7
Jeff He <120319281+jeffhe1@users.noreply.github.com>
2024-08-15 07:59:59 +0000

    update

--- algo/cfd-funding-rate-arbitrage/overnight_fee_bot.py
+++ algo/cfd-funding-rate-arbitrage/overnight_fee_bot.py
@@ -27,23 +27,44 @@ class OvernightFeeBot():
         }
         conn.request("POST", "/api/v1/session", payload, headers)
         res = conn.getresponse()
+        self.sec_token = res.getheader('X-SECURITY-TOKEN')
+        self.cst = res.getheader('CST')
         return res
-    
-    def get_session_detail(self, sec_token, cst):
+
+    def get_session_detail(self):
         conn = http.client.HTTPSConnection(self.base_url)
         payload = ''
         headers = {
-            'X-CAP-ACCESS-TOKEN': sec_token,
-            'CST': cst
+            'X-CAP-ACCESS-TOKEN': self.sec_token,
+            'CST': self.cst
         }
         conn.request("GET", "/api/v1/session", payload, headers)
         res = conn.getresponse()
         data = res.read()
         return data
     
+    def get_markets_id(self):
+        conn = http.client.HTTPSConnection(self.base_url)
+        payload = ''
+        headers = {
+            'X-CAP-ACCESS-TOKEN': self.sec_token,
+            'CST': self.cst
+        }
+        conn.request("GET", "/api/v1/marketnavigation", payload, headers)
+        res = conn.getresponse()
+        data = res.read()
+        return data
+    
+    def get_market_overnight_fees(self, **kwargs) -> list[dict]:
+        market_group: str = kwargs.get('market', None)
+        conn = http.client.HTTPSConnection(self.base_url)
+        payload = ''
+        headers = {
+            'X-CAP-ACCESS-TOKEN': self.sec_token,
+            'CST': self.cst
+        }
 
 bot = OvernightFeeBot('Ap4ukeVKKZY7aVpG', "2211054370abc@gmail.com", 'Abc20030315;', demo=True)
 session = bot.create_new_session()
-SEC_TOKEN = session.getheader('X-SECURITY-TOKEN')
-CST = session.getheader('CST')
-print(bot.get_session_detail(SEC_TOKEN, CST))
+
+print(bot.get_markets_id())
